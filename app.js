282e7a9a3b511469106317bd6d2a0fb26b0ddee6
jamie0816 <jamie020816@gmail.com>
2015-10-14 16:59:12 -1000

    d

--- app.js
+++ app.js
@@ -6,33 +6,41 @@ var press = function() {
 	Uscore++;
 	document.getElementById('score').innerHTML = "Your score: " + Uscore;
 
-	if (Uscore>1000){
+
+	if (Uscore>100000){
+		document.getElementById('next_pet').innerHTML = "<img id='dragon' src='dragon.jpeg'>";
+	}
+	else if (Uscore>1800){
 		document.getElementById('next_pet').innerHTML = "<img id='whaleshark' src='whaleshark.jpg'>";
 		Uscore = Uscore+1000;
 	}
-	else if (Uscore>750){
+	else if (Uscore>1000){
 		document.getElementById('next_pet').innerHTML = "<img id='whale' src='whale.jpg'>";
-		Uscore = Uscore+5;
+		Uscore = Uscore+90;
+	}
+	else if (Uscore>900){
+		document.getElementById('next_pet').innerHTML = "<img id='octopus' src='octopus.jpeg'>";
+		Uscore = Uscore+80
 	}
 	else if (Uscore>600){
 		document.getElementById('next_pet').innerHTML = "<img id='shark' src='shark.jpg'>";
-		Uscore = Uscore+4;
+		Uscore = Uscore+70;
 	}
 	else if (Uscore>400){
 		document.getElementById('next_pet').innerHTML = "<img id='Tiger' src='Tiger.jpg'>";
-		Uscore = Uscore+4;
+		Uscore = Uscore+50;
 	}
 	else if (Uscore>250){
 		document.getElementById('next_pet').innerHTML = "<img id='Lepoard' src='Leopard.jpg'>";
-		Uscore = Uscore+3;
+		Uscore = Uscore+40;
 	}
 	else if (Uscore>175){
 		document.getElementById('next_pet').innerHTML = "<img id='dog' src='dog.jpg'>";
-		Uscore = Uscore+3;
+		Uscore = Uscore+5;
 	}
 	else if (Uscore>100){
 		document.getElementById('next_pet').innerHTML = "<img id='cat' src='cat.jpg'>";
-		Uscore = Uscore+2;
+		Uscore = Uscore+3;
 	}
 	else if (Uscore>50){
 		document.getElementById('next_pet').innerHTML = "<img id='parrots' src='Parrots.jpg'>";
